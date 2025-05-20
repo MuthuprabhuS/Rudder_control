2cb140a26811ab7976c00cc84cb1d525a099b184
MuthuprabhuS <146458833+MuthuprabhuS@users.noreply.github.com>
2025-05-21 00:27:12 +0530

    Add files via upload

---
+++ NIOT_integrated_test/NIOT_integrated_test.ino
@@ -0,0 +1,265 @@
+#define HALL_A 2                                                                       // hall sensor pins:
+#define HALL_B 3
+
+volatile int position = 0;                                                             // hall sensor variables & constants:
+const float pulsesPerRevolution = 14.0;
+const float degreesPerPulse = 360.0 / pulsesPerRevolution;
+volatile unsigned long lastPulseTime = 0;
+float estimatedAngle = 0;
+
+ 
+#define IN1 7                                                                          // motor driver pins:
+#define IN2 8
+#define ENA 9
+
+#include <TinyGPSPlus.h>                                                               // gps pins & libraries:
+#include <SoftwareSerial.h>
+SoftwareSerial gpsSerial(10, 11);
+TinyGPSPlus gps;
+
+double destinationLat = 0.0;                                                           // gps variables:
+double destinationLon = 0.0;
+double currentLat = 0.0;
+double currentLon = 0.0;
+
+#include <Wire.h>                                                                      // imu libraries:
+#include <MPU6050_tockn.h>
+MPU6050 mpu6050(Wire);
+
+const float alpha = 0.98;                                                              // imu variables & constants:
+float pitch = 0, roll = 0, yaw = 0;
+unsigned long prevTime = 0;
+
+const float L = 1.5;                                                                   // glider constants:
+const float V = 0.35;
+const float Cr = 0.3;
+const float Iz = 0.3279;
+const float maxRudderAngle = 51.43;                                                    // we can change to 60 degree
+
+void setup() {
+
+    // hall sensor:
+    pinMode(HALL_A, INPUT);
+    pinMode(HALL_B, INPUT);
+
+    // motor driver:
+    pinMode(IN1, OUTPUT);
+    pinMode(IN2, OUTPUT);
+    pinMode(ENA, OUTPUT);
+
+    // baud rate:
+    Serial.begin(115200);
+    gpsSerial.begin(9600);
+
+    attachInterrupt(digitalPinToInterrupt(HALL_A), hallSensorISR, CHANGE);            // to detect the pulse changes always
+    position = 0;
+    estimatedAngle = 0;
+
+    Serial.println("System Initialized. Starting at 0 degrees.");
+
+    Wire.begin();                                                                    // for i2c communication
+    mpu6050.begin();
+    Serial.println("Calibrating gyroscope... Keep the sensor still.");
+    mpu6050.calcGyroOffsets(true);
+    Serial.println("Calibration complete.");
+
+    prevTime = millis();
+
+    Serial.println("GPS Data Processing Started...");
+    Serial.println("Enter Destination Latitude and Longitude in format: lat lon");
+}
+
+void loop() {
+    while (gpsSerial.available()) gps.encode(gpsSerial.read());
+
+    if (Serial.available()) {
+        char incoming = Serial.peek();
+    
+        // Quit command
+        if (incoming == 'q' || incoming == 'Q') {
+            Serial.read();                                                            // now actually consume the character
+            Serial.println("Quit command received. Returning rudder to 0 degrees...");
+
+            float neutralAngle = 0.0;
+            while (abs(estimatedAngle - neutralAngle) > 0.1) {                        // within rudderTolerance
+                analogWrite(ENA, 255);
+                if (estimatedAngle > neutralAngle) {
+                    digitalWrite(IN1, HIGH);
+                    digitalWrite(IN2, LOW);
+                } else {
+                    digitalWrite(IN1, LOW);
+                    digitalWrite(IN2, HIGH);
+                }
+                delay(10);                                                           // small delay to allow position update
+                updateEstimatedAngle();
+                Serial.println(estimatedAngle);
+            }
+
+            analogWrite(ENA, 0);
+            digitalWrite(IN1, LOW);
+            digitalWrite(IN2, LOW);
+
+            Serial.println("Rudder centered. System halted. Reset Arduino to resume.");
+            
+            while (true);                                                           // infinite loop halts the system until reset
+        }        
+        float lat = Serial.parseFloat();
+        float lon = Serial.parseFloat();
+        if (lat != 0.0 && lon != 0.0) {
+            destinationLat = lat;
+            destinationLon = lon;
+            Serial.print("Destination Set: ");
+            Serial.print(destinationLat, 6);
+            Serial.print(", ");
+            Serial.println(destinationLon, 6);
+        } else {
+            Serial.println("Invalid destination coordinates, check the format.");
+        }
+
+        while (Serial.available()) Serial.read();
+    }
+
+    if (gps.location.isUpdated()) {
+        currentLat = gps.location.lat();
+        currentLon = gps.location.lng();
+
+        Serial.print("Current Position: ");
+        Serial.print(currentLat, 6); Serial.print(", ");
+        Serial.println(currentLon, 6);
+
+        if (destinationLat != 0.0 && destinationLon != 0.0) {
+            double bearing = calculateBearing(currentLat, currentLon, destinationLat, destinationLon);
+            double distance = calculateDistance(currentLat, currentLon, destinationLat, destinationLon);
+
+            Serial.print("Bearing Angle to Destination: ");
+            Serial.println(bearing, 2);
+
+            Serial.print("Distance to Destination (m): ");
+            Serial.println(distance, 2);
+        }
+
+        Serial.println("--------------------------");
+    }
+
+    if (destinationLat != 0.0 && destinationLon != 0.0 && currentLat != 0.0 && currentLon != 0.0) {
+        mpu6050.update();
+
+        unsigned long currentTime = millis();
+        float deltaTime = (currentTime - prevTime) / 1000.0;                    // convert to seconds from milli
+        prevTime = currentTime;
+
+        float accX = mpu6050.getAccX();                                   
+        float accY = mpu6050.getAccY();
+        float accZ = mpu6050.getAccZ();
+        float gyroX = mpu6050.getGyroX();
+        float gyroY = mpu6050.getGyroY();
+
+        float gyroZ = mpu6050.getGyroZ();                                       // gyroZ is used other datas are for future usage
+
+        float accPitch = atan2(accY, sqrt(accX * accX + accZ * accZ)) * 180 / PI;
+        float accRoll  = atan2(-accX, accZ) * 180 / PI;
+
+        pitch = alpha * (pitch + gyroX * deltaTime) + (1 - alpha) * accPitch;
+        roll  = alpha * (roll  + gyroY * deltaTime) + (1 - alpha) * accRoll;
+        yaw  += gyroZ * deltaTime;                                              // yaw is used and other datas are for future usage
+
+        if (yaw < 0) yaw += 360;
+        if (yaw >= 360) yaw -= 360;
+
+        Serial.print(" Yaw: "); Serial.print(yaw, 2);
+
+        updateEstimatedAngle();
+
+
+        double bearing = calculateBearing(currentLat, currentLon, destinationLat, destinationLon);
+        float yawDiff = bearing - yaw;
+        if (yawDiff > 180) yawDiff -= 360;                                      // constrain within +-180 degree to avoid over steering
+        if (yawDiff < -180) yawDiff += 360;
+
+        float Kp = 1.2;                                                         // control the rudder command proportional gain
+        float rudderAngleDeg = constrain(Kp * yawDiff, -maxRudderAngle, maxRudderAngle);       // constrain within the maximum rudder angle
+
+        float yawTolerance = 3.0;                                               // avoid over sensitive
+        float rudderTolerance = 1.5;                                            // avoid over sensitive
+
+        bool yawAligned = abs(yawDiff) <= yawTolerance;                         // true|false so bool
+        bool rudderReached = abs(estimatedAngle - rudderAngleDeg) <= rudderTolerance;
+
+        bool beyondPositiveLimit = (estimatedAngle >= maxRudderAngle && rudderAngleDeg > estimatedAngle);
+        bool beyondNegativeLimit = (estimatedAngle <= -maxRudderAngle && rudderAngleDeg < estimatedAngle);
+
+
+        // control logic to allow reverse from limit:
+
+        if (yawAligned || rudderReached || beyondPositiveLimit || beyondNegativeLimit) {
+            analogWrite(ENA, 0);
+        } else {
+            analogWrite(ENA, 255);
+
+            if (rudderAngleDeg > estimatedAngle) {
+                digitalWrite(IN1, LOW);
+                digitalWrite(IN2, HIGH);
+            } else {
+                digitalWrite(IN1, HIGH);
+                digitalWrite(IN2, LOW);
+            }
+        }
+
+        Serial.print(" | Bearing: "); Serial.print(bearing, 2);                   // result outputs:
+        Serial.print(" | YawDifference: "); Serial.print(yawDiff);
+        Serial.print(" | Rudder Cmd: "); Serial.print(rudderAngleDeg, 2);
+        Serial.print(" deg | Shaft Angle: "); Serial.print(estimatedAngle, 2);
+        Serial.println(" deg");
+
+        
+    }
+}
+
+void updateEstimatedAngle() {
+    estimatedAngle = position * degreesPerPulse;
+
+}
+    
+void hallSensorISR() {
+    bool ha = digitalRead(HALL_A);
+    bool hb = digitalRead(HALL_B);
+
+    if (ha == hb) {                                                              // quadrature encoding
+        position--;
+    } else {
+        position++;
+    }
+
+    lastPulseTime = micros();
+}
+
+double calculateBearing(double lat1, double lon1, double lat2, double lon2) {
+    lat1 = radians(lat1);
+    lon1 = radians(lon1);
+    lat2 = radians(lat2);
+    lon2 = radians(lon2);
+
+    double dLon = lon2 - lon1;
+    double y = sin(dLon) * cos(lat2);
+    double x = cos(lat1) * sin(lat2) - sin(lat1) * cos(lat2) * cos(dLon);
+    double bearing = atan2(y, x);                                               // initial bearing angle
+    bearing = degrees(bearing);                                                 // in degree
+    return (bearing < 0) ? bearing + 360.0 : bearing;                           // to make it only positive
+}
+
+double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
+    const double R = 6371000;
+    lat1 = radians(lat1);
+    lon1 = radians(lon1);
+    lat2 = radians(lat2);
+    lon2 = radians(lon2);
+
+    double dLat = lat2 - lat1;
+    double dLon = lon2 - lon1;
+
+    double a = sin(dLat / 2) * sin(dLat / 2) +
+               cos(lat1) * cos(lat2) * sin(dLon / 2) * sin(dLon / 2);
+
+    double c = 2 * atan2(sqrt(a), sqrt(1 - a));                                 // angular distance
+    return R * c;                                                               // haversine formula
+}
